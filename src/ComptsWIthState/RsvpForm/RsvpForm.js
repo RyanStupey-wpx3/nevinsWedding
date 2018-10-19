b52bfe4d4d18c83879f5110c3b253651faeea2d9
RyanADesigns <[ryan.stupey@gmail.com]>
2018-10-18 22:23:51 -0700

    add and remove guest visuals and functionality

--- src/ComptsWIthState/RsvpForm/RsvpForm.js
+++ src/ComptsWIthState/RsvpForm/RsvpForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './rsvpForm.css'
+import './rsvpForm.css';
+import axios from 'axios';
 
 //person object schema  = {invitedBy: invitee, guest_name: name of guest, status: invite response}
 
@@ -11,7 +12,8 @@ export default class RsvpForm extends Component {
             rsvpTitle, 
             rsvpVerbage, 
             typeOfInput,
-            addedGuest:[],
+            addedGuests:[],
+            stagingGuest: "",
             finalGuestArray:[],
             inviteResponse: 'going',
             alertNotification: "",
@@ -19,6 +21,8 @@ export default class RsvpForm extends Component {
             inputPlaceHolder: "enter your name"
         }
         this.addGuest = this.addGuest.bind(this)
+        this.removeGuest = this.removeGuest.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
         // this.determineInput = this.determineInput.bind(this)
     }
     // determineInput(typeOfInputObj){
@@ -44,48 +48,71 @@ export default class RsvpForm extends Component {
     //         default: 
     //         return(null)
     //     }
+    handleSubmit(event) {
+        event.preventDefault();
+        event.target.value = "";
+      }
 
     addGuest(name){
+        let pushGuest = this.state.addedGuests
        /// if there is no invite rsponse that means there hasnt been any response chosen
         if (!this.state.addedGuests){
             //pushGuest assign to New Array for state array
-            let pushGuest = this.state.addedGuest.push(name)
+            pushGuest.push(name)
             this.setState({
                 //set primary for logging in DB
                 primaryGuest: name,
                 //using addedGuest array for data, rendering under input through map
-                addedGuest: pushGuest,
+                addedGuests: pushGuest,
                 showAddGuestButton: true,
+                stagingGuest:"",
             })
         } else {
-            let pushGuest = this.state.addedGuest.push(name)
+            pushGuest.push(name)
             this.setState({
-                addedGuest: pushGuest,
+                addedGuests: pushGuest,
+                stagingGuest:"",
             })
+            // axios.post('/api/guestInfo', {guest_name: this.state.guestName, guests_added: this.state.guestsAdded, status_of_guests: this.state.inviteResponse, })
         }
-        
-        axios.post('/api/guestInfo', {guest_name: this.state.guestName, guests_added: this.state.guestsAdded, status_of_guests: this.state.inviteResponse, })
+        console.log('this.state.addedGuest', this.state.addedGuests)
     }
+        removeGuest(id){
+            //deconstruct
+            const {addedGuests} = this.state
+            //splice at index (id) 
+              addedGuests.splice(id, 1)
+
+            //set to new variable name
+              let alpha = addedGuests
+
+            //set state to update
+             this.setState({
+                 addedGuests: alpha,
+             })
+            }
+        //next define and write setGuest metho with dynamic object creation and property fill
+    
     
     render() {
-
-        console.log('this.state.guestStatus', this.state.guestStatus)
+        console.log('this.state.addedGuest', this.state.addedGuests)
+        console.log('this.state.stagingGuest', this.state.stagingGuest)
         const {rsvpTitle, rsvpVerbage, typeOfInput} = this.state
-
+        let guestArray=[]
         //iterate through addedGuest; added conditional to return null if there is nothing in addedGuest State array;
-        const guestArray = this.state.addedGuest.map((elem, ind) => {
-            if(this.state.addedGuest.length){
-            for(var i = 0; i < this.state.addedGuest.length; i++){
-            return (<ul>
-                    <li className="guestLi">{elem}</li>
-                    <div className="xDiv">X</div>
-                    </ul>
-                    );
-            }  
-        } else {
-            return null;
-        } 
+        if(this.state.addedGuests.length){
+        guestArray = this.state.addedGuests.map((elem, ind) => {
+            
+            for(var i = 0; i < this.state.addedGuests.length; i++){
+                console.log("ind", ind)
+            return (<li  className="guestLi">{elem} <div onClick={() => this.removeGuest(ind)} className="xDiv">x</div></li>);
+            }
         })
+        } else {
+            
+            console.log('lello');
+        } 
+       console.log('guestArray', guestArray)
         return (
             <div className="rsvpComponentDiv">
                 <div className="rsvpParentDiv">
@@ -94,16 +121,17 @@ export default class RsvpForm extends Component {
                     {/* <form className="rsvpResponseForm">
                 <input type={typeOfInput.types.nameOfType} onChange={() => {this.determineInput(typeOfInput.types.nameOfType)}}/>
                     </form> */}
-                    <form>
-                        <input type="text" onChange={(e)=>{this.addGuest(e.target.value)}} placeholder={this.state.inputPlaceHolder}/> 
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" onChange={(e)=>{this.setState({stagingGuest: e.target.value})}} placeholder={this.state.inputPlaceHolder} value={this.state.stagingGuest}/> 
                             <div className="guestsAdded">
+                            <ul>
                                 {guestArray}
+                            </ul>
                             </div>
-                        <input type="radio" onChange={(e)=>{this.setState({guestStatus: e.target.value})}}/> 
-                        <input type="radio" name="gender" value="Going" checked={this.state.guestStatus === 'Going'}> Yes</input>
-                        <input type="radio" name="gender" value="Maybe" checked={this.state.guestStatus === 'Maybe'}> Maybe </input>
-                        <input type="radio" name="gender" value="No, Sorry" checked={this.state.guestStatus === 'No, Sorry'}>No</input>
-                        <button className="addguestButton" onClick={this.addGuest}>add guest</button>
+                        <input type="radio" name="gender" value="Going" checked={this.state.inviteResponse === 'Going'}/> Yes 
+                        <input type="radio" name="gender" value="Maybe" checked={this.state.inviteResponse === 'Maybe'}/> Maybe
+                        <input type="radio" name="gender" value="No, Sorry" checked={this.state.inviteResponse === 'No, Sorry'}/> Sorry, no
+                        <button className="addguestButton" onClick={() => this.addGuest(this.state.stagingGuest)}>add guest</button>
                     </form>
                 </div>
             </div>
